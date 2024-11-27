c1f49dc39c41664a6a6952dfdc64d20e0bf12927
Lloyd Jones <lloyd@lloydjones.io>
2024-11-27 22:53:51 +0100

    Start FF

--- infra/extension.ts
+++ infra/extension.ts
@@ -1,10 +1,19 @@
 import { bsPoster } from "./api";
 
-new sst.x.DevCommand("BsExtension", {
+new sst.x.DevCommand("BsExtensionChrome", {
   link: [bsPoster],
   dev: {
-    title: "WXT",
+    title: "WXT Chrome",
     directory: "./packages/wxt",
     command: "pnpm dev",
+  },
+});
+
+new sst.x.DevCommand("BsExtensionFirefox", {
+  link: [bsPoster],
+  dev: {
+    title: "WXT Firefox",
+    directory: "./packages/wxt",
+    command: "pnpm dev:firefox",
   },
 });
